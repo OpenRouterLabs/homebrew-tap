fc7b970885236dc6960eba4747cdc056bf9e5f3c
Bjoern Danz <bjoern.danz@openrouter.ai>
2026-08-24 10:32:58 -0700

    ci: automate Ori formula updates

--- Formula/ori.rb
+++ Formula/ori.rb
@@ -1,26 +1,26 @@
 class Ori < Formula
   desc "CLI for running coding agents and building declarative agents"
   homepage "https://github.com/OpenRouterIncubator/ori"
-  version "0.7.1+6fb9ea6"
+  version "0.10.1+b41708f"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/OpenRouterLabs/ori-releases/releases/download/cli-0.7.1-6fb9ea6/ori-darwin-arm64"
-      sha256 "13e698f6154305f1df494c3e6902bbe300f0a23384d0de389001d25edd07f068"
+      url "https://github.com/OpenRouterLabs/ori-releases/releases/download/cli-0.10.1-b41708f/ori-darwin-arm64"
+      sha256 "9c369d8b736f8de9ecb839d903b039a020f1f25abae564682d110a79be29c8f2"
     else
-      url "https://github.com/OpenRouterLabs/ori-releases/releases/download/cli-0.7.1-6fb9ea6/ori-darwin-x64"
-      sha256 "f707c0b9f704ee797d2b7166e484809d9c3cea94a3580648fd672a10d752e4fa"
+      url "https://github.com/OpenRouterLabs/ori-releases/releases/download/cli-0.10.1-b41708f/ori-darwin-x64"
+      sha256 "b79e023fbba449e70ffc8f5006ed1ca908bba988b5d42f66af5358b12713f406"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/OpenRouterLabs/ori-releases/releases/download/cli-0.7.1-6fb9ea6/ori-linux-arm64"
-      sha256 "ac1c0f19367586f463e62dbece2bf8b7e8a416ef3b4391126bf3bb63305f651e"
+      url "https://github.com/OpenRouterLabs/ori-releases/releases/download/cli-0.10.1-b41708f/ori-linux-arm64"
+      sha256 "00fafb101f8b9cad8b575496aef89e47ff3fd74323d51ac97fcc4e50eb6fa044"
     else
-      url "https://github.com/OpenRouterLabs/ori-releases/releases/download/cli-0.7.1-6fb9ea6/ori-linux-x64"
-      sha256 "b03d15fda3d1f9616916bcc0f20b37b70febd654705cf7d29e3152c83d15566a"
+      url "https://github.com/OpenRouterLabs/ori-releases/releases/download/cli-0.10.1-b41708f/ori-linux-x64"
+      sha256 "67a5976416b15ff7d1fc8a77caff10993509d5a8fa904c14e07f87b485f15aa6"
     end
   end
 
